dc52b8fa2e49972a5e7abb63515b735f00882069
J Blaut <jagoda.blaut@bbc.co.uk>
2017-11-28 20:26:55 +0000

    remove the massive if statement, show anything that doesn't match the unpinned regex, update path to morph_modules

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import fs from 'file-system';
-import _ from 'lodash';
+#!/usr/bin/env node --harmony
+
+const fs = require('file-system');
+const _ = require('lodash');
 
 const excludedFiles = ['.DS_Store', '.git', '.gitignore','DO NOT PUT ANYTHING IN THIS DIRECTORY.md','README.md'];
-const pinnedDepRegEx = /^((\")?(\^){0}[0-9]+\.[0-9]+\.[0-9]+(\")?)$/;
-const forkedRegEx = /^(\")?(git\+ssh)/;
+const notPinnedDepRegEx = /^((\")?(\^){1}[0-9]+\.[0-9]+\.[0-9]+(\")?)$/;
 
 fs.readdirSync('../morph-modules').forEach(file => {
   openModule(file);
@@ -11,22 +12,20 @@ fs.readdirSync('../morph-modules').forEach(file => {
 
 function openModule(file) {
     if (excludedFiles.indexOf(file) <= -1) {
-        let packageJson = JSON.parse(fs.readFileSync(`${__dirname}/../../morph-modules/${file}/package.json`, 'utf8'));
+        let packageJson = JSON.parse(fs.readFileSync(`./${file}/package.json`, 'utf8'));
 
         return getDependencies(packageJson);
     }
 }
 
 function getDependencies(packageJson) {
-    if (_.isEmpty(packageJson.dependencies) === false && _.isEmpty(packageJson.devDependencies) === false) {
-        let dependencies = Object.assign(packageJson.dependencies, packageJson.devDependencies);
-
-        return moduleInfo(packageJson.name, dependencies);
-    } else if (_.isEmpty(packageJson.dependencies) === false) {
-        return moduleInfo(packageJson.name, packageJson.dependencies);
-    } else if (_.isEmpty(packageJson.devDependencies) === false) {
-        return moduleInfo(packageJson.name, packageJson.devDependencies);
+    if (_.isEmpty(packageJson.dependencies) && _.isEmpty(packageJson.devDependencies)) {
+        return console.log(`${packageJson.name} has no dependencies :(`);
     }
+
+    let dependencies = Object.assign(packageJson.dependencies || {} , packageJson.devDependencies || {});
+
+    return moduleInfo(packageJson.name, dependencies);
 }
 
 function moduleInfo(moduleName, dependencies) {
@@ -35,13 +34,10 @@ function moduleInfo(moduleName, dependencies) {
     console.log("*** MODULE NAME ***");
     console.log(moduleName);
     console.log("*********");
-    console.log("");
 
     console.log("=== PINNED DEPENDENCY ===");
     deps.forEach(dependency => {
-        if(pinnedDepRegEx.test(dependency[1])) {
-            console.log(`NAME: ${dependency[0]} | VERSION: ${dependency[1]}`);
-        } else if (forkedRegEx.test(dependency[1])) {
+        if(!notPinnedDepRegEx.test(dependency[1])) {
             console.log(`NAME: ${dependency[0]} | VERSION: ${dependency[1]}`);
         }
     });
